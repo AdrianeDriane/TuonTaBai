b3af45f62b0a3cb4a099fd3c6b90f7cf471cb8a8
AdrianeDriane <dilaoaparre@gmail.com>
2025-05-22 15:58:39 +0800

    feat: react-router-dom

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,13 @@
 import { LandingPage } from "./pages/landing/LandingPage"
-
+import { Routes, Route } from "react-router-dom";
+import { AuthPage } from "./pages/auth/AuthPage"
 
 function App() {
   return (
-    <LandingPage/>
+    <Routes>
+      <Route path="/" element={<LandingPage />} />
+      <Route path="/auth" element={<AuthPage />} />
+    </Routes>
   )
 }
 
