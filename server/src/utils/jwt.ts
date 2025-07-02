1e1725e5395e57647988b06a9e6af5762ba0c7af
AdrianeDriane <dilaoaparre@gmail.com>
2025-07-02 12:20:39 +0800

    feat: auth generateAccessToken and generateRefreshToken

--- server/src/utils/jwt.ts
+++ server/src/utils/jwt.ts
@@ -1,7 +1,12 @@
 import jwt from "jsonwebtoken";
 
 const JWT_SECRET = process.env.JWT_SECRET!;
+const JWT_REFRESH_SECRET = process.env.JWT_REFRESH_SECRET!;
 
-export const generateToken = (payload: { id: string; email: string }) => {
-  return jwt.sign({ id: payload.id, email: payload.email }, JWT_SECRET, { expiresIn: "7d" });
+export const generateAccessToken = (payload: { id: string; email: string }) => {
+  return jwt.sign(payload, JWT_SECRET, { expiresIn: "15m" }); // Short-lived
+};
+
+export const generateRefreshToken = (payload: { id: string; email: string }) => {
+  return jwt.sign(payload, JWT_REFRESH_SECRET, { expiresIn: "30d" }); // Long-lived
 };
